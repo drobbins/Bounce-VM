30ca1a6c03295151e3b892fb35990fd32f4d0286
David Robbins <robbins.david@gmail.com>
2014-03-05 15:11:37 -0600

    Added bounce user

--- modules/bounce/manifests/init.pp
+++ modules/bounce/manifests/init.pp
@@ -12,4 +12,19 @@ class bounce {
   # Ensure MongoDB is installed
     require mongodb::server
     require mongodb::client
+
+  # User for to own the Bounce service
+    group { 'services':
+        ensure => present
+    }
+    user { 'bounce':
+        ensure => 'present',
+        name => 'bounce',
+        uid => '701',
+        gid => 'services',
+        home => '/home/bounce',
+        managehome => true,
+        comment => 'Service user for Bounce',
+        password => '$1$hvI2Oipv$xWT8ePfKSXbsptZd3EMgE.'
+    }
 }
