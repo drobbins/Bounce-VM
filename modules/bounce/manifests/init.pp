8d63ba8d45647d91e2a7a7622dd790c4980bd489
David Robbins <robbins.david@gmail.com>
2014-03-05 16:26:43 -0600

    Added git repo and npm install for Bounce

--- modules/bounce/manifests/init.pp
+++ modules/bounce/manifests/init.pp
@@ -1,9 +1,12 @@
 class bounce {
 
+    $bounce_user_home = "/home/bounce"
+    $bounce_home = "${bounce_user_home}/bounce"
+
   # Ensure Node and npm are installed via puppetlabs/nodejs (https://forge.puppetlabs.com/puppetlabs/nodejs)
     require nodejs
 
-  # Install forever (https://www.npmjs.org/package/forever)
+  # Ensure forever (https://www.npmjs.org/package/forever) is installed
     package { 'forever':
         ensure => installed,
         provider => 'npm'
@@ -22,9 +25,26 @@ class bounce {
         name => 'bounce',
         uid => '701',
         gid => 'services',
-        home => '/home/bounce',
+        home => $bounce_user_home,
         managehome => true,
         comment => 'Service user for Bounce',
         password => '$1$hvI2Oipv$xWT8ePfKSXbsptZd3EMgE.'
+    }
+
+  # Ensure Bounce repository is cloned into the bounce users home directory
+    vcsrepo { $bounce_home:
+        ensure => present,
+        provider => git,
+        source => 'https://github.com/agrueneberg/Bounce.git',
+        require => User['bounce']
+    }
+
+  # Ensure the NPM dependencies are installed
+    exec { 'bounce-npm-install':
+        command     => 'npm install',
+        cwd         => $bounce_home,
+        path        => '/usr/local/bin:/usr/bin:/bin',
+        require     => Vcsrepo[$bounce_home],
+        environment => ["HOME=${bounce_user_home}"]
     }
 }
