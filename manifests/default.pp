10330f768b82775a67c311160864cb103aeeb3aa
David Robbins <robbins.david@gmail.com>
2014-03-06 06:49:01 -0600

    Remove unneeded EPEL

--- manifests/default.pp
+++ manifests/default.pp
@@ -11,15 +11,5 @@ package { $packages:
 }
 
 
-# EPEL Packages
-yumrepo { "epel":
-    mirrorlist => "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-${::operatingsystemmajrelease}&arch=${::architecture}",
-    baseurl => 'absent',
-    failovermethod => 'priority',
-    enabled => '1',
-    gpgcheck => '1',
-    gpgkey => 'https://fedoraproject.org/static/0608B895.txt'
-}
-
 # Custom Modules
 include bounce
